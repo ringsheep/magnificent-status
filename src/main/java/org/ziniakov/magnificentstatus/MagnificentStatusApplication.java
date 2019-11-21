77a7b48f62d566871ae4f09e0346244615e37bb3
George Zinyakov <gzinyakov@alfabank.ru>
2019-11-21 09:18:20 +0300

    initial commit

---
+++ src/main/java/org/ziniakov/magnificentstatus/MagnificentStatusApplication.java
@@ -0,0 +1,13 @@
+package org.ziniakov.magnificentstatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagnificentStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagnificentStatusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ziniakov/magnificentstatus/MagnificentStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ziniakov.magnificentstatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagnificentStatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
